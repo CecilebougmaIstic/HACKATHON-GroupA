717c3c9a0a5c56215a642f0168a38620e8dd7a29
GuillaumeSUBRA <guillaumesubra1@gmail.com>
2021-11-30 11:43:35 +0100

    update back end

---
+++ projet/backend/src/main/java/HACKATON/backend/HACKATON.java
@@ -0,0 +1,15 @@
+package HACKATON.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HACKATON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(HACKATON.class, args);
+    }
+}
